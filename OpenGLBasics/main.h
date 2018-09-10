99b81310c19c3e3c8b0fffc4fdfa05cfcdcc7157
PabloNarvaja <pnarvaja.21@gmail.com>
2018-09-10 18:42:37 -0300

    Intanced with uniforms array

--- OpenGLBasics/main.h
+++ OpenGLBasics/main.h
@@ -10,9 +10,7 @@
 #include <iostream>
 #include <GLM/glm.hpp>
 #include <GLM/gtx/transform.hpp>
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
+
 
 typedef unsigned int uint32;
 
@@ -132,6 +130,7 @@ public:
     {
         glUniformMatrix4fv(getLocation(name), 1, GL_FALSE, &matrix[0][0]);
     }
+
 private:
     int getLocation(const char* name) const
     {
@@ -187,171 +186,6 @@ struct Material
     float shininess;
 };
 
-struct Vertex
-{
-    glm::vec3 pos;
-    glm::vec3 normal;
-    glm::vec2 uvCoord;
-};
-
-class Mesh
-{
-    uint32 VAO; // Vertex Array Object
-    uint32 VBO; // Vertex Buffer Object
-    //uint32 IBO; // Per Instance attributes Buffer Object
-    uint32 EBO; // Elements Buffer Object
-public:
-    std::vector<Vertex> vertices;
-    std::vector<uint32> indices;
-public:
-    Mesh(const std::vector<Vertex>& nVertices, const std::vector<uint32>& nIndices)
-        : vertices(nVertices), indices(nIndices)
-    {
-        // Generate the buffers
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glGenBuffers(1, &EBO);
-        
-        // Bind the Array Object
-        glBindVertexArray(VAO);
-        
-        
-        
-        // Bind Vertex Buffer to the Array Object
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        // Reserve memory and Send data to the Vertex Buffer
-        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
-        
-        
-        // Bind Element Buffer to the Array Object
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        // Reserve memory and Send data to the Elment Buffer
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
-            &indices[0], GL_STATIC_DRAW);
-
-        
-
-        // Set the atribute pointers
-        // vertex positions
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-        // vertex normals
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
-        // vertex texture coords
-        glEnableVertexAttribArray(2);
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, uvCoord));
-    }
-
-    void draw(Shader& shader)
-    {
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, (uint32) indices.size(), GL_UNSIGNED_INT, 0);
-    }
-};
-
-class Model
-{
-    std::vector<Mesh> meshes;
-    std::string directory;
-    std::vector<Material>* materials;
-public:
-    Model(const std::string& path, std::vector<Material>* inMaterials = nullptr)
-        : materials(inMaterials)
-    {
-        loadModel(path);
-    }
-
-    void draw(Shader& shader)
-    {
-        if (materials && (*materials).size() > 0)
-            for (uint32 i=0; i < meshes.size(); i++)
-            {
-                if ((*materials)[i].diffuse)
-                    (*materials)[i].diffuse->bind(0);
-                if ((*materials)[i].specular)
-                    (*materials)[i].specular->bind(1);
-                shader.setFloat("material.shininess", (*materials)[i].shininess);
-                meshes[i].draw(shader);
-            }
-        else
-            for (uint32 i = 0; i < meshes.size(); i++)
-            {
-                meshes[i].draw(shader);
-            }
-    }
-private:
-    void loadModel(const std::string& path)
-    {
-        Assimp::Importer import;
-        const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
-
-        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
-        {
-            std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
-            return;
-        }
-        directory = path.substr(0, path.find_last_of('/'));
-
-        processNode(scene->mRootNode, scene);
-    }
-    void processNode(aiNode *node, const aiScene *scene)
-    {
-        // process all the node's meshes (if any)
-        for (unsigned int i = 0; i < node->mNumMeshes; i++)
-        {
-            aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
-            meshes.push_back(processMesh(mesh, scene));
-        }
-        // then do the same for each of its children
-        for (unsigned int i = 0; i < node->mNumChildren; i++)
-        {
-            processNode(node->mChildren[i], scene);
-        }
-    }
-    Mesh processMesh(aiMesh *mesh, const aiScene *scene)
-    {
-        std::vector<Vertex> vertices;
-        std::vector<unsigned int> indices;
-
-        for (unsigned int i = 0; i < mesh->mNumVertices; i++)
-        {
-            Vertex vertex;
-            // process vertex positions, normals and texture coordinates
-            glm::vec3 vector;
-            vector.x = mesh->mVertices[i].x;
-            vector.y = mesh->mVertices[i].y;
-            vector.z = mesh->mVertices[i].z;
-            vertex.pos = vector;
-
-            vector.x = mesh->mNormals[i].x;
-            vector.y = mesh->mNormals[i].y;
-            vector.z = mesh->mNormals[i].z;
-            vertex.normal = vector;
-
-            if (mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
-            {
-                glm::vec2 vec;
-                vec.x = mesh->mTextureCoords[0][i].x;
-                vec.y = mesh->mTextureCoords[0][i].y;
-                vertex.uvCoord = vec;
-            }
-            else
-                vertex.uvCoord = glm::vec2(0.0f, 0.0f);
-            vertices.push_back(vertex);
-        }
-        
-        // process indices
-        for (unsigned int i = 0; i < mesh->mNumFaces; i++)
-        {
-            aiFace face = mesh->mFaces[i];
-            for (unsigned int j = 0; j < face.mNumIndices; j++)
-                indices.push_back(face.mIndices[j]);
-        }
-        
-        return Mesh(vertices, indices);
-    }
-};
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
--- OpenGLBasics/res/vertexInstanced.glsl
+++ OpenGLBasics/res/vertexInstanced.glsl
@@ -2,13 +2,15 @@
 layout(location = 0)in vec3 position;
 layout(location = 1)in vec3 normal;
 layout(location = 2)in vec2 texCoord;
-layout(location = 3)in mat4 transform;
+
+
+uniform mat4 transforms[256];
 
 out vec2 uvCoord;
 
 void main()
 {
     vec4 pos = vec4(position, 1.0);
-    gl_Position = transform * pos;
+    gl_Position = transforms[gl_InstanceID] * pos;
     uvCoord = texCoord;
 }
